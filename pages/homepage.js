ae6d40358a3eadb0f58845831d36f2c392fd3bb8
SusmitaK1997 <root@LAPTOP-QPOHGR6L.localdomain>
2022-11-03 17:21:50 +0530

    allow done

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,12 +1,16 @@
+import { Router } from "next/router";
+import { symbol } from "prop-types";
 import React from "react";
 import { useState } from "react";
 const abi=require('../abi.json');
+const abi1=require('../abi1.json'); 
 import Web3 from 'web3';
-
-//  import { ethers } from "ethers";
+import { useRouter } from 'next/router';
+import { setTimeout } from "timers";
 
 
 function App() {
+  const router = useRouter();
  const [tokenAddress,setTokenAddress] = useState('');
   const handleSubmit=(e)=>{
     e.preventDefault();
@@ -15,28 +19,52 @@ function App() {
     console.log("Token : " +token,"\n");
   }
   const handleChange=async(e)=>{
-    //  const provider = new ethers.providers.Web3Provider(window.ethereum);
-     const web3 = new Web3('https://ds2.exx.network/');
+   
+     const web3 = new Web3('https://bsc-dataseed1.binance.org/');
     try{
       const tokenContract = new web3.eth.Contract(abi, tokenAddress);
       const symbol = await tokenContract.methods.symbol().call();
       const name = await tokenContract.methods.name().call();
       if(symbol){
-        console.log('valid',name);
+        
+        if(checkLpToken()==true){
+          console.log('lp token');
+          
+        }
       }
-      else
-      {
-        console.log('not valid');
-      }
+     
+    else {
+      console.log('valid token',name);
+    }
     }
     catch(err){
-      console.log(err);
+      console.log('not valid');
 
     }
   }
-  
-        
-        return (
+  const checkLpToken=async( )=>{
+    
+    const web3 = new Web3('https://bsc-dataseed1.binance.org/');
+    try{
+      const tokenContract = new web3.eth.Contract(abi1, tokenAddress);
+      const t1 = await tokenContract.methods.token0().call();
+      const t2 = await tokenContract.methods.token1().call();
+      if(t1 && t2 ){
+        console.log('lp token');
+        setTimeout(()=>{
+        router.push('/');
+        },2000);
+      }
+      return true;
+    }
+    catch(err){
+      console.log('not lp token');
+      alert("not lp token");
+
+    }
+  }
+   
+    return (
             <div className="Homepage">
             <h1>
                 Hello Homepage
@@ -45,11 +73,10 @@ function App() {
               <div className="input-group">
                 <label htmlFor="token">tokeninfo</label>
                 <input type="text" value = {tokenAddress} id="tokeninfo" onChange={(e)=>setTokenAddress(e.target.value)}  />
-                
-              </div>
-              <button type="proceed" className="proceed-btn" onClick={handleChange}>
+              <button type="proceed" className="proceed-btn" onClick={handleChange} >
                 Proceed
-              </button>
+                </button>
+              </div> 
             </form>
             
           </div>
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,61 @@
 import React from "react";
+import Web3 from 'web3';
+import { useState } from "react";
+const abi1=require('../abi1.json');
 
 function App1() {
+   const [amount,setamount] = useState('');
   const handleSubmit=(e)=>{
     e.preventDefault();
     const Lock=e.target.lock1.value;
     const Time=e.target.time1.value;
     console.log("Amount to lock : " +Lock,"\n","Liquidity unlock time : " +Time);
   }
+    const check=async(e)=>{
+   
+      const web3 = new Web3('https://bsc-dataseed1.binance.org/');
+      
+      
+     try{
+        const lptoken_ADDRESS= '0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE';
+        const spenderAddr= '0xd1b508360a1286d5A4071a67224Be387d85cb75E';
+        const Contract = new web3.eth.Contract(abi1, lptoken_ADDRESS);
+        const owner = '0x763702343200F8f851450EA514DD7488BFd2d408';
+      
+       const allow = await Contract.methods.allowance(spenderAddr, owner).call();
+       
+       
+       if(allow>0){
+        console.log('allowence',allow);
+       }
+       else{
+       await Contract.methods.approve(spenderAddr, amount).send({ from: owner });
+        console.log('Approave');
+       }
+       
+       
+      }
+     catch(err){
+       console.log('error');
+ 
+     }
+   
+  }
         return (
             <div className="app">
             <form onSubmit={handleSubmit}>
               <div className="input-group">
                 <label htmlFor="Lock">Amount to lock</label>
-                <input type="text" id="Amount to lock" name="lock1"/>
+                <input type="text" value = {amount} id="Amount to lock" name="lock1" onChange={(e)=>setamount(e.target.value)}/>
               </div>
               <div className="input-group">
                 <label htmlFor="time">Liquidity unlock time</label>
-                <input type="text" id="Liquidity unlock time" name="time1"/>
+                <input type="text"  id="Liquidity unlock time" name="time1" />
               </div>
-              <button type="submit" className="submit-btn">
+              <button type="submit" className="submit-btn" onClick={check}>
                 Submit
               </button>
-              <button type="approave" className="approave-btn">
+              <button type="approave" className="approave-btn" >
                
                 Approave
               </button>
